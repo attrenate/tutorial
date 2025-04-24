0b32f38772c947e2fda4e7f0167c8b1ce4f6ef1e
attrenate <nikolozghomidzevol100@gmail.com>
2025-04-24 20:17:38 +0400

    node

---
+++ modules.js
@@ -0,0 +1,12 @@
+// Modules
+const john = 'john'
+const peter = 'peter'
+const secret = 'SUPER_SECRET'
+
+const sayHi = (name) =>{
+    console.log(`Hello there ${name}`);
+}
+
+sayHi('susan');
+sayHi(john);
+sayHi(peter);
